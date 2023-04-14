6b180d80096b3a922dbf74a306cb3362cb21d026
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-04-14 17:29:32 +0000

    Made some new changes

--- src/pages/Enterprise.jsx
+++ src/pages/Enterprise.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Img from "../images/virtual_card.png";
+import Question from "../images/question.png";
+import "./Enterprise.css";
 
 const Enterprise = () => {
   return (
@@ -8,32 +11,40 @@ const Enterprise = () => {
         <Navbar />
         <div className="section">
 
-        <div className="">
-            <h2>What is Guava technologies Solutions for Business Enterprise </h2>
-            <p>
-                Guava Technology Solutions for Business ENTERPRISE
-                Guava Technology Solutions Enterprise is an operating system for investment professionals that powers every aspect of the investment process.
-                This end-to-end portfolio management software combines sophisticated risk analytics with comprehensive portfolio management tools, trading, operations, compliance and accounting tools on a single platform.
-                With a single, standardized data set, Guava Technology Solutions brings clarity, efficiency, scalability, and cost savings to the entire investment lifecycle. Guava technologies Solutions native risk analysis makes Guava Technology Solutions a truly end-to-end platform.
-            </p>
+        <div className="enterprise">
+            <div className="enterprise-img">
+                <img src={Img} alt="" />
+            </div>
+            <div className="enterprise-text">
+                <h2>What is Guava technologies Solutions for Business Enterprise </h2>
+                <p>
+                    Guava Technology Solutions for Business ENTERPRISE
+                    Guava Technology Solutions Enterprise is an operating system for investment professionals that powers every aspect of the investment process.
+                    This end-to-end portfolio management software combines sophisticated risk analytics with comprehensive portfolio management tools, trading, operations, compliance and accounting tools on a single platform.
+                    With a single, standardized data set, Guava Technology Solutions brings clarity, efficiency, scalability, and cost savings to the entire investment lifecycle. Guava technologies Solutions native risk analysis makes Guava Technology Solutions a truly end-to-end platform.
+                </p>
+            </div>
         </div>
 
-        <div className="">
-            <h2>What types of securities does Guava Technology Solutions analyze? </h2>
-            <p>
-            Guava Technology Solutions is a multi-asset class platform that supports risk analytics and the full investment process across equity, fixed income, FX, bank loans, derivatives and alternatives such as real estate, commodities, and private equity.
-            </p>
+        <div className="securities">
+            <div className="enterprise-text">
+                <h2>What types of securities does Guava Technology Solutions analyze? </h2>
+                <p>
+                    Guava Technology Solutions is a multi-asset class platform that supports risk analytics and the full investment process across equity, fixed income, FX, bank loans, derivatives and alternatives such as real estate, commodities, and private equity.
+                </p>
+            </div>
+
+            <div className="securities-img">
+                <img src={Question} alt="" />
+            </div>
         </div>
 
-        <div className="">
+        <div className="business">
             <h2>How Guava Technologies for BUSINESS ENTERPRISE works </h2>
             <p>
             Guava Technology Solutions for BUSINESS ENTERPRISE  drives Collective Intelligence by providing tools to help your organization communicate effectively, address problems quickly, and make informed decisions at every step of the investment process.
             Embedded in our platform is :
             Risk Management tools which provides tools to help you understand portfolio risk and performance across asset classes to support informed investment decisions. Our flexible user-configurable reports and interactive what-if analysis tools seek to meet the needs of risk managers, portfolio managers, compliance officers, executives, investment committees and boards.
-
-
-
             As an end-to-end investment platform, Guava Technologies  is:
             Guava Technologies and Guava app platform are relied on by approximately 2k+ investment professionals around the world.
             Powered by a handful of collectively 96+ years of investing and developers experience , focused on continuous enhancements that are made available to all clients.
--- src/pages/Guava.js
+++ src/pages/Guava.js
@@ -1,43 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "./Guava.css";
+import Stack from "../images/stack.png";
 
 const Guava = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div>
       <Navbar />
       <div className="section">
         <div className="head">
-          <h2>Welcome to the Guava App Wallet</h2>
-          <div>
+          <h2>Welcome to the Guava App Wallet 🤗</h2>
+          <p>
             The Guava Wallet app is a self-custody wallet that’s your portal to
             web3, where you can swap crypto with no network fees on Polygon.
             Guava Wallet gives you full control over your funds on the Polygon
             and Ethereum networks, which means you hold and maintain the private
             key to your assets. Stay tuned, because we’ll be adding more
             networks to guava Wallet in the future, too.
-          </div>
+          </p>
         </div>
         <div className="steps">
           <div className="steps--item">
-            <h2>How do I create a New Wallet</h2>
+            <h2>How do I create a New Wallet?</h2>
             <p>Create a New Wallet</p>
-            <ul>
-              <li>Select Create a new wallet</li>
-              <li>Select Use Face ID/Touch ID or Create a custom PIN</li>
+            <ul className="new-wallet">
+              <li><i class="fa-solid fa-arrows-up-down-left-right ic"></i> Select Create a new wallet</li>
+              <li><i class="fa-solid fa-arrows-up-down-left-right ic"></i> Select Use Face ID/Touch ID or Create a custom PIN</li>
               <li>
+              <i class="fa-solid fa-arrows-up-down-left-right ic"></i>
                 Select Back it up to iCloud, Back it up manually, or Skip to
                 back it up later
               </li>
-              <li>Select Continue or Skip to claim your reward later</li>
+              <li><i class="fa-solid fa-arrows-up-down-left-right ic"></i>Select Continue or Skip to claim your reward later</li>
               <li>
+                <i class="fa-solid fa-arrows-up-down-left-right ic"></i>
                 Fund your wallet through guava Crypto or from another wallet or
                 exchange
               </li>
-              <li>Select Open wallet</li>
+              <li><i class="fa-solid fa-arrows-up-down-left-right ic"></i>Select Open wallet</li>
               <li>
                 <span className="warning">
-                  We strongly recommend backing up your wallet because it's the
+                  NB: We strongly recommend backing up your wallet because it's the
                   only way you can recover your wallet and crypto if you lose or
                   reset your device. If you don’t back up your wallet when you
                   create it, you can do it later in Backups & security. For
@@ -54,24 +61,24 @@ const Guava = () => {
               it to guava Wallet by using your existing wallet’s secret recovery
               phrase.
             </p>
-            <ul>
-              <li>Select Import your existing wallet</li>
+            <ul className="existing-wallet">
+              <li><i class="fa-solid fa-arrows-to-dot ic"></i>Select Import your existing wallet</li>
               <li>
-                You can either copy and paste, or manually enter your secret
+              <i class="fa-solid fa-arrows-to-dot ic"></i>You can either copy and paste, or manually enter your secret
                 recovery phrase
               </li>
-              <li>Select Import</li>
-              <li>Select Use Face/Touch ID or Create a custom PIN</li>
+              <li><i class="fa-solid fa-arrows-to-dot ic"></i>Select Import</li>
+              <li><i class="fa-solid fa-arrows-to-dot ic"></i>Select Use Face/Touch ID or Create a custom PIN</li>
               <li>
-                Select Back it up to iCloud, Back it up manually, or Skip to
+              <i class="fa-solid fa-arrows-to-dot ic"></i>Select Back it up to iCloud, Back it up manually, or Skip to
                 back it up later
               </li>
-              <li>Select Continue or Skip to claim your reward later</li>
+              <li><i class="fa-solid fa-arrows-to-dot ic"></i>Select Continue or Skip to claim your reward later</li>
               <li>
-                Fund your wallet through guava Wallet or from another wallet or
+              <i class="fa-solid fa-arrows-to-dot ic"></i>Fund your wallet through guava Wallet or from another wallet or
                 exchange
               </li>
-              <li>Select Open wallet</li>
+              <li><i class="fa-solid fa-arrows-to-dot ic"></i>Select Open wallet</li>
             </ul>
           </div>
 
@@ -81,23 +88,24 @@ const Guava = () => {
               If you backed up a wallet to your iCloud in the past, you can
               recover it.
             </p>
-            <ul>
-              <li>Select Recover your wallet from iCloud</li>
-              <li>Enter the password to unlock your backup</li>
-              <li>Select Recover wallet</li>
-              <li>Select Use Face/Touch ID or Create a custom PIN</li>
-              <li>Select Continue</li>
+            <ul className="recover-wallet">
+              <li><i class="fa-solid fa-arrow-right-from-bracket ic"></i>Select Recover your wallet from iCloud</li>
+              <li><i class="fa-solid fa-arrow-right-from-bracket ic"></i>Enter the password to unlock your backup</li>
+              <li><i class="fa-solid fa-arrow-right-from-bracket ic"></i>Select Recover wallet</li>
+              <li><i class="fa-solid fa-arrow-right-from-bracket ic"></i>Select Use Face/Touch ID or Create a custom PIN</li>
+              <li><i class="fa-solid fa-arrow-right-from-bracket ic"></i>Select Continue</li>
             </ul>
           </div>
         </div>
 
         <div className="disclosure">
-          <h2>Fees and disclosures</h2>
-          <div className="disclosure-item">
+          <div>
+            <h2>Fees and disclosures</h2>
+            <img src={Stack} alt="stack of money"/>
+          </div>
+          <p className="disclosure-item">
             Guava app currently does not charge service fees on token swaps.
             Fees are subject to change.
-          </div>
-          <div className="disclosure-item">
             Network fees, or gas fees, are blockchain transaction processing
             fees. These required fees are submitted to the network along with
             your transaction. The fee fluctuates based on the volume of
@@ -105,12 +113,15 @@ const Guava = () => {
             network. Network fees may apply for token approvals, transfers to an
             external wallet, and interacting with dapps. guava Wallet doesn’t
             collect any part of the network fee.
-          </div>
+          </p>
         </div>
 
         <div className="protection">
-          <h2>How do I Protect my wallet</h2>
-          <div className="protection-item">
+          <div className="protection-header">
+            <h2>How do I Protect my wallet</h2>
+            { isOpen? <i class="fa-solid fa-lock-open protection-icon"></i>:<i class="fa-solid fa-lock protection-icon"></i>}
+          </div>
+          <div className="protection-text">
             To secure your wallet, you need to set up either Face/Touch ID or a
             custom PIN, which you’ll use to authenticate every time you open the
             guava Wallet app. We recommend Face/Touch ID as the most secure way
@@ -118,23 +129,29 @@ const Guava = () => {
             unique. If you want to switch between Face/Touch ID or a PIN, you
             can change your security settings any time in Backups & security.
           </div>
-          <div className="protection-item">
-            Set up Face/Touch ID When prompted during setup, tap OK to allow
-            guava Wallet to use Face/Touch ID.
-          </div>
-          <div className="protection-item">
-            Custom PIN When prompted during setup, enter a 6-digit PIN and then
-            confirm the PIN a second time. Remember to never share this number
-            with anyone.
-          </div>
-          <div className="protection-item">
-            Secret recovery phrase A secret recovery phrase, also known as a
-            seed phrase, is a series of words generated by your wallet that
-            allows you access to your wallet and crypto.
-          </div>
+          <ul>
+            <li className="protection-item">
+              <i class="fa-solid fa-check-double ic"></i>
+              Set up Face/Touch ID When prompted during setup, tap OK to allow
+              guava Wallet to use Face/Touch ID.
+            </li>
+            <li className="protection-item">
+              <i class="fa-solid fa-check-double ic"></i>
+              Custom PIN When prompted during setup, enter a 6-digit PIN and then
+              confirm the PIN a second time. Remember to never share this number
+              with anyone.
+            </li>
+            <li className="protection-item">
+              <i class="fa-solid fa-check-double ic"></i>
+              Secret recovery phrase A secret recovery phrase, also known as a
+              seed phrase, is a series of words generated by your wallet that
+              allows you access to your wallet and crypto.
+            </li>
+          </ul>
+          
           <div className="protection-item">
             <span className="warning">
-              It’s important to keep your secret recovery phrase stored in a
+              NB: It’s important to keep your secret recovery phrase stored in a
               safe place and never share it with anyone. We don’t have access to
               this phrase, so we can’t help you recover it if you forget it. And
               we’ll never ask you to input your recovery phrase anywhere except
@@ -144,52 +161,54 @@ const Guava = () => {
         </div>
 
         <div className="charges">
-          <div className="charges-item">
-            <h2>Deposits</h2>
-            <p>
+          <div className="charges-deposits">
+            <h2 className="charges-deposits__title">Deposits</h2>
+            <p className="charges-deposits__text">
               Guava does not charge fees to deposit money into your spending or
               brokerage account, regardless of the type of account you're
               transferring from.
             </p>
           </div>
 
-          <div className="charges-item">
-            <h2>Withdrawals</h2>
+          <div className="charges-withdrawals">
+            <h2 className="charges-withdrawals__title">Withdrawals</h2>
             <p>
               When withdrawing money from your spending or brokerage account, it
               depends on what type of account you’re transferring money to:
             </p>
-            <ul>
-              <li>
-                <h3>Bank account</h3>
-                <p>No fee for withdrawals</p>
-              </li>
-              <li>
+            <ul className="withdrawals-sections">
+              <li className="withdrawals-sections__left">
                 <h3>External debit card account</h3>
                 <p>
                   Withdrawals have a 1.5% fee based on the amount being
                   transferred out. You’ll see the calculated fee when you
                   initiate the withdrawal, so you’ll know the amount up front.
                 </p>
+
+              </li>
+              <li className="withdrawals-sections__right">
+               <h3>Bank account</h3>
+                <p>No fee for withdrawals</p>
               </li>
             </ul>
           </div>
         </div>
 
         <div className="invest">
-          <h2>What do I have through invest today?</h2>
-          <p>
+          <h2 className="invest-title">What do I have through invest today?</h2>
+          <p className="invest-text">
             invest is offered through your brokerage account, and you have
             access to the following features.
           </p>
           <ul>
-            <li>Interest on uninvested brokerage cash held in the account</li>
-            <li>FDIC Insurance for up to GHS1M </li>
+            <li><i class="fa-solid fa-arrows-turn-right ic"></i>Interest on uninvested brokerage cash held in the account</li>
+            <li><i class="fa-solid fa-arrows-turn-right ic"></i>FDIC Insurance for up to GHS1M </li>
             <li>
+            <i class="fa-solid fa-arrows-turn-right ic"></i>
               Routing and account number that you can use for direct deposit
             </li>
-            <li>Debit card</li>
-            <li>Fee-free ATMs</li>
+            <li><i class="fa-solid fa-arrows-turn-right ic"></i>Debit card</li>
+            <li><i class="fa-solid fa-arrows-turn-right ic"></i>Fee-free ATMs</li>
           </ul>
         </div>
       </div>
