df814d9b9c7816eb86f6f5966b103f2a54ed827a
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-04-17 14:17:41 +0000

    Added stuff

--- src/components/Globe.js
+++ src/components/Globe.js
@@ -1,21 +1,22 @@
 import React from "react";
 import GlobeImg from "../images/globe.png";
+import "./Globe.css";
 
 const Globe = () => {
   return (
-    <div className="globe-container section">
-        <div className="globe-text">
-          <h2 className="globe-text__title">
-            Where the world builds software
-          </h2>
-          <p className="globe-text__content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ex placeat, aliquam nesciunt at libero laboriosam odio reprehenderit. Nam, officia.
-          </p>
-        </div>
-        <div className="globe-img">
-          <img src={GlobeImg} alt="" className="globe"/>
-        </div>
-    </div>
+        <section className="globe-container">
+          <div className="globe-text">
+            <h2 className="globe-text__title">
+              Where the world builds software
+            </h2>
+            <p className="globe-text__content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ex placeat, aliquam nesciunt at libero laboriosam odio reprehenderit. Nam, officia.
+            </p>
+          </div>
+          <div className="globe-img">
+            <img src={GlobeImg} alt="" className="globe-img__image"/>
+          </div>
+        </section>
   );
 };
 
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,52 +1,73 @@
 import React from 'react'
 import './Company.css'
+import CompanyImg from "../images/Company.png";
+import People from "../images/people.png";
+import MoneyTree from "../images/money_tree.png";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const Company = () => {
   return (
-    <div className="companies_wrapper">
-        
-        <Navbar />
-        
-        <section className="companies_content">
+    <div className="companies">
+
+      <Navbar />
+
+      <section className="companies-content">
+        <div className="companies-intro">
+          <p className="companies-intro__text">
             Guava Technologies is a financial asset manager and investing
             infrastructure platform leveraging cutting edge technologies
             for individuals and businesses.
-            Millions in the world can use guava;
+          </p>
+          <div className="companies-intro__img">
+            <img src={CompanyImg} alt="" />
+          </div>
+        </div>
+
+        <div className="companies-people">
+          <div className="companies-people-img">
+            <img src={People} alt="" />
+          </div>
+          <div className="companies-people-content">
+            <h2 className="companies-people__title">Millions in the world can use <span>Guava</span>:</h2>
+            <ul className="companies-people__reasons">
+              <li className="companies-people__item"><i class="fa-solid fa-arrow-right"></i>To invest in the international securities market</li>
+              <li className="companies-people__item"><i class="fa-solid fa-arrow-right"></i>Enhance international cross border and local payments</li>
+              <li className="companies-people__item"><i class="fa-solid fa-arrow-right"></i>Store high decentralized digital assets</li>
+              <li className="companies-people__item"><i class="fa-solid fa-arrow-right"></i>Grow their revenue to enhance more business opportunities</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="companies-enterprise">
+          <div className="companies-enterprise__head">
+            <h2> Guava Enterprise</h2>
+            <img src={MoneyTree} alt="" />
+            <p>
+              Enterprise is an operating system for investment professionals that powers every aspect of the investment process
+            </p>
+          </div>
+
+          <div className="companies-enterprise-list">
             <ul>
-              <li>To invest in the international securities market</li>
-              <li>Enhance international cross border and local payments</li>
-              <li>Store high decentralized digital assets</li>
-              <li>Grow their revenue to enhance more business opportunities</li>
+              <li><i class="fa-solid fa-arrows-spin"></i>The end to end portfolio management software combines sophisticated risk analytics with comprehensive portfolio management tools , trading , operations, compliance and accounting tools on a single platformwith a single standardized data set. 
+              </li>
+              <li>
+              <i class="fa-solid fa-arrows-spin"></i>Enterprise brings clarity , efficiency, scalability & cost saving to the entire investment cycle
+              </li>
+              <li>
+                <i class="fa-solid fa-arrows-spin"></i>Enterprise drives collective intelligence by powering tools to help your organisation communicate effectively, address problems quickly and make informed decisions at every step of the investment process
+              </li>
+              <li>
+                <i class="fa-solid fa-arrows-spin"></i>Enterprise supports business process design and implementation, 3rd party data sources and middle level operations, data management and system configuration services
+              </li>
             </ul>
+          </div>
+        </div>
+      </section>
 
-            <div className="enterprise">
-              <div className="enterprise-head">
-                <h2> Guava Enterprise</h2>
-                <p>
-                Enterprise is an operating system for investment professionals that powers every aspect of the investment process
-                </p>
-              </div>
+      <Footer />
 
-              <ul>
-                <li>The end to end portfolio management software combines sophisticated risk analytics with comprehensive portfolio management tools , trading , operations, compliance and accounting tools on a single platform
-                </li>
-                <li>
-                with a single standardized data set , Enterprise brings clarity , efficiency, scalability & cost saving to the entire investment cycle
-                </li>
-                <li>
-                Enterprise drives collective intelligence by powering tools to help your organisation communicate effectively, address problems quickly and make informed decisions at every step of the investment process
-                </li>
-                <li>
-                Enterprise supports business process design and implementation, 3rd party data sources and middle level operations, data management and system configuration services
-                </li>
-            </ul>
-            </div>
-        </section>
-        
-        <Footer />
-        
     </div>
   )
 }
