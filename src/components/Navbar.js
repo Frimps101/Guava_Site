e965655b09b245e991975fa06a46535aba3575c7
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-04-21 10:37:34 +0000

    Had to redo header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,33 +11,78 @@ const Navbar = () => {
   const closeMobileMenu = () => setClick(false);
 
   return (
-    <header className="nav_container section">
-      <Link to="/" className="logo">
-        <img src={Logo} alt="" className="logo-img"/>
-      </Link>
+    <>
+      <header className="nav-header">
+        <div className="nav-header__left">
+          <div className="">
+            <img src={Logo} alt="" className="logo-img" />
+          </div>
 
-      {/* <div className="menu-icon" onClick={handleClick}>
-        <i className={click ? "fas fa-times" : "fas fa-bars"} />
-      </div> */}
+          <div className="header-dropdowns">
+            <div class="dropdown">
+              <button class="dropbtn">Product</button>
+              <div class="dropdown-content">
+                <a href="#">Guava app</a>
+                <a href="#">Guava wallet</a>
+                <a href="#">Card</a>
+                <a href="#">Guava Wealth</a>
+              </div>
+            </div>
+          </div>
 
-      <nav className={click ? "nav-menu active" : "nav-menu"}>
-        <Link to="/company" className="nav_item">
-          Company
-        </Link>
-        <Link to="/enterprise" className="nav_item">
-          Enterprise
-        </Link>
-        <Link to="/guava" className="nav_item">
-          Guava App
-        </Link>
-        <Link to="/contact" className="nav_item">
-          Contact
-        </Link>
-        <Link to="/faqs" className="nav_item">
-          FAQ
-        </Link>
-      </nav>
-    </header>
+          <div className="header-dropdowns">
+            <div class="dropdown">
+              <button class="dropbtn">Business</button>
+              <div class="dropdown-content">
+                <a href="#">Brokerage firms</a>
+                <a href="#">Banks</a>
+                <a href="#">SMEs</a>
+                <a href="#">Global Business</a>
+                <a href="#">Pricing</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="header-dropdowns">
+            <div class="dropdown">
+              <button class="dropbtn">Developers</button>
+              <div class="dropdown-content">
+                <a href="#">Documentation</a>
+                <a href="#">Wallet SDK</a>
+                <a href="#">Payments</a>
+                <a href="#">Exchange API</a>
+                <a href="#">Power Automated Trading</a>
+                <a href="#">Integrate your Blockchain</a>
+                <a href="#">Plugins</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="header-dropdowns">
+            <div class="dropdown">
+              <button class="dropbtn">Company</button>
+              <div class="dropdown-content">
+                <a href="#">About Us</a>
+                <a href="#">Careers</a>
+                <a href="#">Support</a>
+                <a href="#">Blog</a>
+                <a href="#">Security</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="nav-header__right">
+          <div className="nav-header__right">
+            Pricing
+          </div>
+          <div className="nav-header__right">
+            Sign In
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
